5bf30ac8e1c0fec4f05d118fbfb3305982fe3f5c
Julien Fabre <julien.fabre@tubemogul.com>
2014-05-06 22:44:49 -0700

    First commit

---
+++ lib/yaml-lint.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+module Logging
+  ESCAPES = { :green  => "\033[32m",
+              :yellow => "\033[33m",
+              :red    => "\033[31m",
+              :reset  => "\033[0m" }
+
+  def info(message)
+    emit(:message => message, :color => :green)
+  end
+
+  def warn(message)
+    emit(:message => message, :color => :yellow)
+  end
+
+  def error(message)
+    emit(:message => message, :color => :red)
+  end
+
+  def emit(opts={})
+    color   = opts[:color]
+    message = opts[:message]
+    print ESCAPES[color]
+    print message
+    print ESCAPES[:reset]
+    print "\n"
+  end
+end
+
+class YamlLint
+  include Logging
+
+  def initialize(file)
+    @file = file
+    @error = false
+  end
+
+  def do_lint
+    unless File.exists? @file
+      error "File #{@file} does not exist"
+      @error = true
+      return
+    end
+    if File.directory? @file
+      self.parse_directory @file
+    else
+      self.parse_file @file
+    end
+    @error
+  end
+
+  def parse_directory(directory)
+    return nil if ['.', '..'].include? directory
+    Dir.glob("#{directory}/*").each do |fdir|
+      if File.directory? fdir
+        self.parse_directory fdir
+      else
+        self.parse_file fdir
+      end
+    end
+    nil
+  end
+
+  def parse_file(file)
+    unless File.extname(file) == ".yaml"
+      error "The extension of the file #{file} should be .yaml"
+      return
+    end
+    begin
+      YAML.load_file(file)
+    rescue Exception => err
+      error "File : #{file}, error: #{err}"
+      @error = true
+    else
+      info "File : #{file}, Syntax OK"
+    end
+    nil
+  end
+end
---
+++ yaml-lint.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'yaml-lint'
+  s.version     = '0.0.1'
+  s.date        = '2014-05-06'
+  s.summary     = 'Really simple YAML lint'
+  s.description = 'Check if your YAML files can be loaded.'
+  s.authors     = ['Julien Fabre']
+  s.email       = 'ju.pryz@gmail.com'
+  s.files       = ['lib/yaml-lint.rb', 'bin/yaml-lint']
+  s.license     = 'MIT'
+end
