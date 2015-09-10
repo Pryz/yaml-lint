3a43f01482e9c4f576b7fdaf49f197ad1c83e5d8
Hiroki Yoshida <h-yoshida@mediba.jp>
2015-09-10 18:06:01 +0900

    update spec

--- spec/yamllint_spec.rb
+++ spec/yamllint_spec.rb
@@ -25,19 +25,19 @@ describe 'YamlLint' do
     it 'writes OK and error when not quiet' do
       lint = YamlLint.new(FIXTURES_PATH)
       expect { lint.do_lint }.to output(/Syntax OK/).to_stdout
-      expect { lint.do_lint }.to output(/error/).to_stdout
+      expect { lint.do_lint }.to output(/error/).to_stderr
     end
 
     it 'does write only errors when quiet' do
       lint = YamlLint.new(FIXTURES_PATH, {:quiet => true})
       expect { lint.do_lint }.to_not output(/Syntax OK/).to_stdout
-      expect { lint.do_lint }.to output(/error/).to_stdout
+      expect { lint.do_lint }.to output(/error/).to_stderr
     end
 
     it 'does not write anything when very quiet' do
       lint = YamlLint.new(FIXTURES_PATH, {:veryquiet => true})
       expect { lint.do_lint }.to_not output(/Syntax OK/).to_stdout
-      expect { lint.do_lint }.to_not output(/error/).to_stdout
+      expect { lint.do_lint }.to_not output(/error/).to_stderr
     end
   end
 
